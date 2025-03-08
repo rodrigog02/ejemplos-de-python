96363f1b71bb472031c32f6895082e9bb7014dff
rodrigog02 <zs23013937@estudiantes.uv.mx>
2025-03-07 18:47:38 -0600

    Update uso del bucle for en python.py
    se agrega codigo

--- uso del bucle for en python.py
+++ uso del bucle for en python.py
@@ -1,1 +1,5 @@
-
+# Ejemplo de bucle for en Python
+# se crea un bucle que va desde el número 1 hasta el número 5 (el 6 no está incluido).
+for i in range(1, 6):
+    print(i)
+# imprime cada numero en una nueva linea
