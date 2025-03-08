7471bcf85b2de511d0b76fb893d5d49a0685ba92
rodrigog02 <zs23013937@estudiantes.uv.mx>
2025-03-07 18:50:52 -0600

    Create uso del bucle while en python.py
    se agrega codigo

---
+++ uso del bucle while en python.py
@@ -0,0 +1,6 @@
+# Ejemplo de bucle while en Python
+# Inicializamos la variable i
+i = 1 
+while i <= 5: #el bucle se ejecutará mientras i sea menor o igual a 5.
+    print(i)  # Imprimimos el valor actual de i
+    i += 1  # Incrementamos i en 1
